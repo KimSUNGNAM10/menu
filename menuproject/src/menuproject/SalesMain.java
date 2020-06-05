d4b3f846b82499b47b19d520c4d7b87811a4345d
admin <admin@YD03-10>
2020-06-05 17:40:42 +0900

    s

--- menuproject/src/menuproject/SalesMain.java
+++ menuproject/src/menuproject/SalesMain.java
@@ -10,7 +10,7 @@ public class SalesMain extends Application{
 
 	@Override
 	public void start(Stage primaryStage) throws Exception {
-		Parent root = FXMLLoader.load(getClass().getResource("salesChart.fxml"));
+		Parent root = FXMLLoader.load(getClass().getResource("SalesMenuChart.fxml"));
 		Scene scene = new Scene(root);
 		primaryStage.setScene(scene);
 		primaryStage.show();
