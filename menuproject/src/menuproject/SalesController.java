da16657679a3de19ffcb5fc8c13e04d0ce3e26b5
admin <sm121913@gmail.com>
2020-06-09 12:10:28 +0900

    da

--- menuproject/src/menuproject/SalesController.java
+++ menuproject/src/menuproject/SalesController.java
@@ -10,7 +10,7 @@ public class SalesController implements Initializable{
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
 		// TODO Auto-generated method stub
-		
+		 
 	}
 
 }
