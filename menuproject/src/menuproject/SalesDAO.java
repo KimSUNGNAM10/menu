a91e8abcb09529189db04547a0637eb95a193e57
admin <sm121913@gmail.com>
2020-06-09 10:04:11 +0900

    aaa

--- menuproject/src/menuproject/SalesDAO.java
+++ menuproject/src/menuproject/SalesDAO.java
@@ -38,7 +38,7 @@ public class SalesDAO {
 	
 	//메뉴이름, 가격을 받아서 메뉴를 추가함.
 	public void insertMenu(String menuName, int price) {
-		String sql = "insert into menu(name, price) values(?, ?)";
+		String sql = "insert into menu values(menu_id, name, price) values(menu_seq.NEXTVAL, ?, ?)";
 		
 		try {
 			PreparedStatement pstmt = conn.prepareStatement(sql);
@@ -61,11 +61,39 @@ public class SalesDAO {
 		if(conn == null)
 			return;
 		
+		//insert into sales_history(order_no, menu_id) values(sales_history_seq.NEXTVAL, 1);
 	}
+	
 	
 	public void buyMenuFromName(String name) {
 		if(conn == null)
 			return;
+	}
+	
+	public void insertDummyData(int days) {
+		//특정 구간에 random개의 상품이 판매되는 더미 데이터 생성.
+		for(int i=0; i<days; ++i) {
+			double randOrderCount = Math.random() * 100;
+			//하루 0~100개의 주문이 들어옴
+			for(int j=0; j<randOrderCount; ++j) {	
+				int randMenu = (int)(Math.random() * 10) + 1;
+				String sql = String.format("insert into sales_history(order_no, sale_date, menu_id) "
+						+ "values(sales_history_seq.nextval, sysdate-%d, %d)"
+						, i, randMenu);
+				
+				PreparedStatement pstmt;
+				try {
+					pstmt = conn.prepareStatement(sql);
+					ResultSet rs = pstmt.executeQuery();
+					int r = pstmt.executeUpdate();
+					System.out.println(r + "건 입력됨.");
+				} catch (SQLException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				
+			}
+		}
 	}
 	
 	public List<SalesMenu> getMenuList() {
